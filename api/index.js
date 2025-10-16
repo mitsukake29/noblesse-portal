8ab26c62cdc0e1f699f650fa06416c41d9b9626d
mitsukake29 <mitsuisho.general@gmail.com>
2025-10-16 14:52:39 +0900

    Fix Vercel serverless function configuration for API

--- api/index.js
+++ api/index.js
@@ -1,3 +1,8 @@
+// Vercel serverless function handler
 const app = require('../backend/server');
 
+// Export the Express app as a Vercel serverless function
 module.exports = app;
+
+// Also export a handler function for Vercel
+module.exports.default = app;
